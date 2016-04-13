901526e312efb7c8094487a31bb55ffe7d6b70e4
Daniel O'Donnell <dan@dvoc.ca>
2016-04-13 16:47:14 -0400

    move to dep service

--- src/Chuck.js
+++ src/Chuck.js
@@ -12,7 +12,7 @@ var Chuck = {
             jsOut = Chuck._concatinate(config.sources, distKey);
         }
 
-        jsOut = '(function(root,factory){ if (typeof module === "object" && module.exports) { module.exports = factory(); } else { root["'+config.products.jsObj+'"] = factory(); } })(this,function(){' + jsOut + ' return G["'+config.products.jsEndpoint+'"]; });';
+        jsOut = '(function(root,factory){ if (typeof module === "object" && module.exports) { module.exports = factory(); } else { root["'+config.products.jsObj+'"] = factory(); } })(this,function(){' + jsOut + ' return g.use("'+config.products.jsEndpoint+'"); });';
 
         fs.writeFile(config.paths.js + '/' + config.products.js, jsOut);
 
@@ -34,7 +34,8 @@ var Chuck = {
 
         //TODO generalize this to various platforms
 
-        var buffer = "var G = {};\n";
+        var buffer = "var G = function(){this._libs={}}; G.prototype={add:function(key,obj){this._libs[key]=obj;}, use:function(key){return this._libs[key];}};\n";
+        buffer += "var g = new G();";
 
         for (var i=0; i<files.length; i++) {
 
@@ -54,10 +55,10 @@ var Chuck = {
 
                     contents = contents.replace("export default", "return");
 
-                    var preStr = "G['" + files[i].name + "'] = (function(",
-                        appStr = '})(';
+                    var preStr = "g.add('"+files[i].name+"', (function(__dep){",
+                        appStr = "})(g));\n";
 
-                    var innerVars = [],
+                    /*var innerVars = [],
                         innerRefs = [];
 
                     if (files[i].deps) {
@@ -71,7 +72,7 @@ var Chuck = {
                     appStr += innerRefs.join(',');
 
                     preStr += '){';
-                    appStr += ");\n";
+                    appStr += ");\n";*/
 
                     buffer += preStr + contents + appStr;
 
@@ -79,7 +80,7 @@ var Chuck = {
 
                     if (files[i].setFromGlobal && files[i].name) {
 
-                        buffer += contents + "\n" + 'G["'+files[i].name+'"] = ' + files[i].setFromGlobal + ";";
+                        buffer += contents + "\n" + 'g.add("'+files[i].name+'", ' + files[i].setFromGlobal + ");";
 
                     } else {
 
